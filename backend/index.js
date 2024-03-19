ec208271b6f077ebd47bfb51240065ef995e8bd2
sasankvarma0 <sasankvarma32@gmail.com>
2024-03-19 10:06:44 -0500

    added add book api endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,19 @@
 const dotenv = require("dotenv");
-dotenv.config({ path: __dirname + '/.env' });
+dotenv.config({ path: __dirname+'/.env' });
 require("./mongoose")
 
 
-const express = require("express");
-const app = express();
+const express=require("express");
+const app=express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const cors = require("cors");
+const cors=require("cors");
 
-const allowedDomains = ["http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://localhost:3003"]
-
-app.use(cors({
-    credentials: true,
-    origin: function (origin, callback) {
+const allowedDomains=["http://localhost:3000","http://localhost:3001","http://localhost:3002","http://localhost:3003"]
+// app.use(cors());
+app.use(cors({  
+    credentials:true,
+    origin:function (origin, callback) {
         // bypass the requests with no origin
         if (!origin) return callback(null, true);
         if (allowedDomains.indexOf(origin) === -1) {
@@ -22,21 +22,22 @@ app.use(cors({
         }
         return callback(null, true);
     },
-    exposedHeaders: ['Content-Disposition']
+    exposedHeaders:['Content-Disposition']
 }));
 
-const authRoutes = require("./routes/authRoutes");
-app.use("/auth", authRoutes);
-
 const librarianRoutes = require("./routes/librarianRoutes");
+const authRoutes = require("./routes/authRoutes");
+
 app.use("/librarian",librarianRoutes);
+app.use("/auth",authRoutes);
 
 
-app.all("/*", (req, res) => {
+
+app.all("/*",(req,res)=>{
     res.send("page not found");
 });
 
 //port listening at in the server
-app.listen(3002, () => {
+app.listen(3002, ()=>{
     console.log("running on port " + 3002);
 });
--- backend/routes/librarianRoutes.js
+++ backend/routes/librarianRoutes.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const {bookUpload} = require("../multer");
 
+const librarianController = require("../controllers/librarianControllers");
+
+router.post("/addBook/:isbnNumber",bookUpload,librarianController.addBook );
 
 module.exports = router;
