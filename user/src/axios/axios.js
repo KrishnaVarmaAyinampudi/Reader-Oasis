a8e4800e693e58b9608ee4c52fbab5763b1ea1ad
DhondiAshutoshVarma <43470769+Aashu1999@users.noreply.github.com>
2024-04-29 17:11:48 +0000

    resolved axios file error

--- user/src/axios/axios.js
+++ user/src/axios/axios.js
@@ -1,10 +1,32 @@
 import axios from "axios";
 
+async function getPublicIp() {
+  try {
+    const response = await axios.get('http://169.254.169.254/latest/meta-data/public-ipv4');
+    return response.data.trim();  // Remove any leading/trailing whitespace
+  } catch (error) {
+    console.error('Error getting public IP:', error);
+    return null;
+  }
+}
+let ipAddress;
+getPublicIp()
+  .then(ip => {
+    if (ip) {
+      ipAddress = ip;
+    } else {
+      console.log('Public IP retrieval failed.');
+    }
+  });
 
-const BASE_URL = "http://localhost:3002" 
+
+
+const BASE_URL = `http://${ipAddress}:3002`
 
 const axiosInstance=axios.create({
     baseURL:BASE_URL,
 });
 
 export default axiosInstance; 
+
+
